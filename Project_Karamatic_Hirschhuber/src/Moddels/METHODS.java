3910a5fdea78f9d7ba17301714a4123e9e0b4c44
iAteYourCouch <sekaramatic@tsn.at>
2022-03-25 12:40:34 +0100

    worked on gamemenu

--- Project_Karamatic_Hirschhuber/src/Moddels/METHODS.java
+++ Project_Karamatic_Hirschhuber/src/Moddels/METHODS.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class METHODS {
     static Scanner reader = new Scanner(System.in);
 //int HP, int Mana, int speed, int atkspeed, int mr, int ar, int ap, int ad, boolean aggresive, String name, String firstAblity, String secondAbility
-    public Champion create_champ() {
+    public static Champion create_champ() {
         Champion c = new Champion();
         System.out.println("Please enter Champion Name:");
         c.setName(reader.nextLine());
@@ -40,26 +40,48 @@ public class METHODS {
         System.out.println("Beenden[B]");
         System.out.print("Ihre Wahl: ");
         choice = reader.next().toLowerCase().charAt(0);
+        System.out.print("\n\n");
 
         switch(choice){
-            case 'G':
-
+            case 'g':
+                System.out.println("Spielerkampf");
                 break;
 
-            case 'M':
-
+            case 'm':
+                System.out.println("Monster bekämpfen");
                 break;
 
-            case 'P':
-
+            case 'p':
+                System.out.println("Shop");
                 break;
 
-            case 'C':
+            case 'c':
+                System.out.println("Champion laden[L]");
+                System.out.println("Champion erstellen[C]");
+                System.out.println("Champion löschen[D]");
+                System.out.print("Ihre Wahl: ");
+                choice = reader.next().toLowerCase().charAt(0);
+                System.out.print("\n\n");
 
+                switch(choice){
+                    case 'l':
+                        System.out.println("Wählen Sie Ihren Champion aus!");
+                        break;
+                    case 'c':
+                        create_champ();
+                        break;
+                    case 'd':
+                        System.out.println("Wählen Sie Ihren Champion aus!");
+                        break;
+                }
                 break;
 
-            case 'B':
+            case 'b':
                 System.out.println("Auf Wiedersehen!");
+                break;
+
+            default:
+                System.out.println("Falsche Taste!");
                 break;
         }
     }
